d8a08cfd4d4943ac1c9475f7ea39157612deb3cf
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 16:39:42 +0700

    Add files via upload

---
+++ Mengetahui segitiga sama sisi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,b,c;
+
+cout<<"Menentukan segitiga sama sisi"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+cout<<"masukan panjang sisi a"<<endl;
+cin>>a;
+cout<<"masukan panjang sisi b"<<endl;
+cin>>b;
+cout<<"masukan panjang sisi c"<<endl;
+cin>>c;
+
+if(a==b && a==c)
+    cout<<"Segitiga tersebut sama sisi:";
+    else
+        cout<<"Segitiga tersebut TIDAK sama sisi:";
+    return 0;
+}
